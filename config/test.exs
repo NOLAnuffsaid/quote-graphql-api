b1dcbe3446b5f05c6154143430071769f17f6c94
W Millon <wjmillon@gmail.com>
2019-09-25 20:16:44 -0500

    config update

--- config/test.exs
+++ config/test.exs
@@ -1,13 +1,5 @@
 use Mix.Config
 
-# Configure your database
-config :elixir_quotes, Quotes.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "elixir_quotes_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :elixir_quotes_web, QuotesWeb.Endpoint,
