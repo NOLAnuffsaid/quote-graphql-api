da50bd92b8b2b88549aca53f72c2cc55242cac52
W Millon <wjmillon@gmail.com>
2019-09-28 14:04:47 -0500

    initial seed data code

--- apps/elixir_quotes/priv/repo/seeds.exs
+++ apps/elixir_quotes/priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+gen_originator_name_params = fn ->
+  web_name = &Faker.Internet.user_name/0
+  full_name = &Faker.Name.name/0
+  first_name = &Faker.Name.first_name/0
+  poke_name = &Faker.Pokemon.name/0
+  star_wars_name = &Faker.StarWars.character/0
+  super_name = &Faker.Superhero.name/0
+
+  func = Enum.random [web_name, full_name, first_name, poke_name, star_wars_name, super_name]
+  %{:name => func.()}
+end
+
+
+gen_quote_text = fn ->(4..10) |> Enum.random() |> Faker.Lorem.sentence() end
+gen_quote_params = fn t -> %{:quote_text => t} end
+
+
+
+
+
